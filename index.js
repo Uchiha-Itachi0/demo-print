e4eafb78cb24ddd7551571870c0a81ce7f047289
Anubhav Shukla <anubhav008shukla@gmail.com>
2023-02-19 18:40:06 +0530

    bottom design 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const menu = document.getElementsByClassName('menu')[0];
+const menu = document.getElementsByClassName('bottom')[0];
 menu.addEventListener('click', rotateIt);
 function rotateIt() {
     menu.classList.toggle('rotate');
